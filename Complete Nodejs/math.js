54504172dcd1bae5bdd561bf2db44447e5d2c807
shakilmahammud <hshakil987@gmail.com>
2022-01-06 19:41:55 +0600

    node js

---
+++ Complete Nodejs/math.js
@@ -0,0 +1,16 @@
+const add = (a,b) => a+b
+
+const sub = (a,b) => a-b
+
+const test = (a,b) => add(a,b)/sub(a,b)
+
+//module .exports.test = test ;
+
+//module.exports = {
+    //test
+//}
+
+//module exports help us for access globaly others file
+
+
+
