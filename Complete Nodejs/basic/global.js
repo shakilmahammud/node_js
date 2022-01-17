b4954ae4c69f7eb86488343cca4845c063765749
shakilmahammud <hshakil987@gmail.com>
2022-01-17 23:56:04 +0600

    fun

--- Complete Nodejs/basic/global.js
+++ Complete Nodejs/basic/global.js
@@ -2,3 +2,31 @@
 const test = require('./math')
 
 console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
+//use module use case
+const test = require('./math')
+
+console.log(test.test(20,10)) //ans = 30
