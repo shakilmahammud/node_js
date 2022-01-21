36b8c5ce41142e1941a4cc190d23c8372a795f64
shakilmahammud <hshakil987@gmail.com>
2022-01-21 14:59:39 +0600

    middleware practice

--- Complete Nodejs/Node Packege/server.js
+++ Complete Nodejs/Node Packege/server.js
@@ -1,7 +1,20 @@
 const express = require('express')
-
+const morgan = require('morgan')
 const app = express();
+const PORT = process.env.PORT || 8000
 
+// app.use(express.json())
+const customMiddleware = (req,res,next)=>{
+    console.log('middleware work')
+    next()
+}
+app.use(morgan('dev'))
 
-app.use(express.json())
-
+app.get('/',(req,res)=>{
+    res.json({
+        message:'hello'
+    })
+})
+app.listen(PORT,()=>{
+    console.log(`server is running on PORT ${PORT} `)
+})
