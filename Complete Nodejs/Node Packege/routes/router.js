00465afaeffe78b08dac0277e80576672fa1171f
shakilmahammud <hshakil987@gmail.com>
2022-02-04 23:36:33 +0600

    update

--- Complete Nodejs/Node Packege/routes/router.js
+++ Complete Nodejs/Node Packege/routes/router.js
@@ -57,6 +57,68 @@ router.get('/user',(req,res)=>{
 router.put('/user',(req,res)=>{
     res.send('update page')
 })
+const express = require('express')
+const router = express.Router()
+
+
+router.get('/login',(req,res)=>{
+    res.send('login page')
+})
+router.post('/login',(req,res)=>{
+    res.send('put page')
+})
+
+router.put('/login',(req,res)=>{
+    res.send('update page')
+})
+router.delete('/login',(req,res)=>{
+    res.send('delete page')
+})
+router.get('/login',(req,res)=>{
+    res.send('login page')
+})
+router.post('/login',(req,res)=>{
+    res.send('put page')
+})
+
+router.put('/login',(req,res)=>{
+    res.send('update page')
+})
+router.delete('/login',(req,res)=>{
+    res.send('delete page')
+})
+
+router.post('/user',(req,res)=>{
+    res.send('update page')
+})
+router.get('/user',(req,res)=>{
+    res.send('update page')
+})
+router.put('/user',(req,res)=>{
+    res.send('update page')
+})
+router.post('/user',(req,res)=>{
+    res.send('update page')
+})
+router.get('/user',(req,res)=>{
+    res.send('update page')
+})
+router.put('/user',(req,res)=>{
+    res.send('update page')
+})
+
+router.post('/user',(req,res)=>{
+    res.send('update page')
+})
+router.get('/user',(req,res)=>{
+    res.send('update page')
+})
+router.put('/user',(req,res)=>{
+    res.send('update page')
+})
+
+
+
 
 
 
